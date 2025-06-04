1cf4ea8be2f8c3546986916083f25603e0af0c4b
root <lena.gonzalez-breton@euroserum.com>
2025-06-04 11:11:53 +0200

    tailwind.config.js: config color scheme, font and spacings

--- tailwind.config.js
+++ tailwind.config.js
@@ -4,7 +4,30 @@ module.exports = {
     "./app/**/*.{js,ts,jsx,tsx}", // ← pour Expo Router
   ],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        cream: "#F2E9DE",
+        coffee: "#2E1900",
+        charcoal: "#1F1F23",
+        mocha: "#684C44",
+        white: "#FFFFFF",
+        black: "#000000",
+      },
+      fontFamily: {
+        satoshi: ["Satoshi", "System", "sans-serif"],
+      },
+      borderRadius: {
+        xl: "1rem",
+        "2xl": "1.5rem",
+      },
+      spacing: {
+        xs: "4px",
+        sm: "8px",
+        md: "16px",
+        lg: "24px",
+        xl: "32px",
+      },
+    },
   },
   plugins: [],
 }
